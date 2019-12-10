b455abe6e4cc61931dec2b090bbdea029f1582ae
Sorin Raduta <raduta.sorin@yahoo.com>
2019-12-10 14:44:30 +0200

    feature: polybar + multimonitor workspaces

--- .scripts/statusbar.sh
+++ .scripts/statusbar.sh
@@ -6,7 +6,15 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch Polybar, using default config location ~/.config/polybar/config
-polybar disconnect &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload disconnect &
+  done
+else
+  polybar --reload disconnect &
+fi
+
+# # Launch Polybar, using default config location ~/.config/polybar/config
+# polybar disconnect &
 
 echo "Polybar launched..."
