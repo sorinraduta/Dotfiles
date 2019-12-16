6316bcfbc535824a9b47d90e1d739a2f19f1db79
Sorin Raduta <raduta.sorin@yahoo.com>
2019-12-16 14:57:24 +0200

    patch: hackspeed keyboard id

--- .scripts/polybar-scripts/info-hackspeed.sh
+++ .scripts/polybar-scripts/info-hackspeed.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 # shellcheck disable=SC2016,SC2059
-# Tried: 3, 5,
-KEYBOARD_ID="5"
+# Tried: 3, 5, 10,
+KEYBOARD_ID="10"
 
 # cpm: characters per minute
 # wpm: words per minute (1 word = 5 characters)
 METRIC=cpm
 FORMAT="# %d $METRIC"
 
-INTERVAL=20
+INTERVAL=5
 
 # If you have a keyboard layout that is not listed here yet, create a condition
 # yourself. $3 is the key index. Use `xinput test "AT Translated Set 2 keyboard"`
