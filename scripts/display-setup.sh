b45450b3b63fb984ab0d4887eebaf4e4241a1b71
Sorin Raduta <raduta.sorin@yahoo.com>
2019-12-22 22:06:16 +0200

    feature: lightdm resolution; volnoti

--- scripts/display-setup.sh
+++ scripts/display-setup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 {%@@ if profile == "disconnect" @@%}
 xrandr --newmode "1920x1200_60.00"  193.25  1920 2056 2256 2592  1200 1203 1209 1245 -hsync +vsync
 xrandr --addmode DVI-I-1 1920x1200_60.00
