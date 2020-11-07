073e0a4643635f4908a6a34cf89572ba0d811ccf
Sorin Raduta <raduta.sorin@yahoo.com>
2020-11-07 11:45:50 +0200

    patch: organized folders

--- dotfiles/scripts/display-setup.sh
+++ dotfiles/scripts/display-setup.sh
@@ -2,13 +2,6 @@
 
 # TODO: switch display_layout
 xrandr --newmode "1920x1200_60.00"  193.25  1920 2056 2256 2592  1200 1203 1209 1245 -hsync +vsync
-xrandr --addmode DVI-0 1920x1200_60.00
-xrandr --output DVI-0 --primary --mode 1920x1200_60.00 --pos 1080x635 --rotate normal \
---output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate left \
---output DVI-1 --off \
---output DisplayPort-0 --off
-
-# xrandr --output DP-1 --mode 1920x1080 --pos 0x420 --rotate normal \
-# --output HDMI-1 --mode 1920x1080 --pos 1920x0 --rotate right \
-# --output DVI-I-1 --off \
-# --output DVI-D-1 --off
+xrandr --addmode {{@@ main_monitor @@}} 1920x1200_60.00
+xrandr --output {{@@ main_monitor @@}} --primary --mode 1920x1200_60.00 --pos 1080x635 --rotate normal \
+--output {{@@ secondary_monitor @@}} --mode 1920x1080 --pos 0x0 --rotate left
--- dotfiles/scripts/screenshot.sh
+++ dotfiles/scripts/screenshot.sh
@@ -2,7 +2,6 @@
 
 f=/tmp/$(date +%s).png
 
-# maim -s $f
 deepin-screenshot -s $f
 
 cat $f | xclip -selection clipboard -t image/png
